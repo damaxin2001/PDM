ea2e73213b81ba478f8b9b1b54d8a626549fdcfa
Lourenço <leto.lg@gmail.com>
2024-10-21 18:24:03 +0100

    first commit

---
+++ Calculator/app/src/main/java/ipca/examples/calculator/CalculatorBrain.kt
@@ -0,0 +1,41 @@
+package ipca.examples.calculator
+
+class CalculatorBrain {
+
+    enum class Operation (val op: String) {
+        SUM("+"),
+        SUB("-"),
+        MULT("*"),
+        DIV("/"),
+        SQRT("√"),
+        SIGNAL("±"),
+        PERCENT("%"),
+        RAND("\uD83D\uDE02");
+
+        companion object {
+            fun getOp(value: String): Operation {
+                return entries.find { it.op == value } ?: RAND
+            }
+        }
+    }
+
+    var operation : Operation? = null
+    var operand : Double = 0.0
+
+    fun doOperation(value: Double) : Double {
+        val result = when (operation) {
+            Operation.SUM -> operand + value
+            Operation.SUB -> operand - value
+            Operation.MULT -> operand * value
+            Operation.DIV -> operand / value
+            Operation.SQRT -> Math.sqrt(operand)
+            Operation.SIGNAL -> -operand
+            Operation.PERCENT -> operand / 100
+            Operation.RAND -> Math.random()
+            null -> value
+        }
+        return result
+    }
+
+
+}
---
+++ Calculator/app/src/main/java/ipca/examples/calculator/CalculatorView.kt
@@ -0,0 +1,196 @@
+package ipca.examples.calculator
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ipca.examples.calculator.ui.components.CalcButton
+import ipca.examples.calculator.ui.theme.CalculatorTheme
+import ipca.examples.calculator.ui.theme.Orange
+import ipca.examples.calculator.ui.theme.Pink40
+
+@Composable
+fun CalculatorView(modifier: Modifier = Modifier ){
+
+    var display by remember { mutableStateOf("0") }
+
+    var calculatorBrain = remember { CalculatorBrain() }
+
+    fun getDisplay() : Double {
+        return display.toDouble()
+    }
+
+    fun setDisplay(value: Double) {
+        if ( value % 1 == 0.0) {
+            display = value.toInt().toString()
+        } else {
+            display = value.toString()
+        }
+    }
+
+    var userIsInTheMiddleOfTyping by remember {
+        mutableStateOf(false)
+    }
+
+    val onNumPress : (String) -> Unit = { num ->
+        if(userIsInTheMiddleOfTyping) {
+            if (display == "0") {
+                if (num == ".") {
+                    display = "0."
+                } else {
+                    display = num
+                }
+            } else {
+                if (num == ".") {
+                    if (!display.contains(".")) {
+                        display += num
+                    }
+                } else {
+                    display += num
+                }
+            }
+        }else{
+            display = num
+        }
+
+        userIsInTheMiddleOfTyping = true
+    }
+
+    val onOperationPressed : (String) -> Unit = { op ->
+        userIsInTheMiddleOfTyping = false
+        setDisplay( calculatorBrain.doOperation(getDisplay()))
+        calculatorBrain.operand = getDisplay()
+        calculatorBrain.operation = CalculatorBrain.Operation.getOp(op)
+    }
+
+    Column (modifier = modifier){
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = display,
+            textAlign = TextAlign.End,
+            style = MaterialTheme.typography.displayLarge)
+        Row{
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = "7",
+                onButtonPress = onNumPress
+            )
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = "8",
+                onButtonPress = onNumPress
+            )
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = "9",
+                onButtonPress = onNumPress
+            )
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = "+",
+                isOperation = true,
+                onButtonPress = onOperationPressed
+            )
+
+        }
+        Row{
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = "4",
+                onButtonPress = onNumPress
+            )
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = "5",
+                onButtonPress = onNumPress
+            )
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = "6",
+                onButtonPress = onNumPress
+            )
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = "-",
+                isOperation = true,
+                onButtonPress = onOperationPressed
+            )
+
+        }
+        Row{
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = "1",
+                onButtonPress = onNumPress
+            )
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = "2",
+                onButtonPress = onNumPress
+            )
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = "3",
+                onButtonPress = onNumPress
+            )
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = "*",
+                isOperation = true,
+                onButtonPress = onOperationPressed
+            )
+
+        }
+        Row{
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = "0",
+                onButtonPress = onNumPress
+            )
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = ".",
+                onButtonPress = onNumPress
+            )
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = "=",
+                isOperation = true,
+                onButtonPress = onOperationPressed
+            )
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = "/",
+                isOperation = true,
+                onButtonPress = onOperationPressed
+            )
+
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CalculatorPreview(){
+    CalculatorTheme {
+        CalculatorView()
+    }
+
+}
